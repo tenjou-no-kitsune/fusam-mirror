9e22dfa35e6c6720ba98984f7dbb6f6f5cab61e9
Jean-Baptiste Emmanuel Zorg <zorgjeanbe@proton.me>
2026-09-20 13:54:50 +0200

    Fix updates and localhost matching

--- loader.user.js
+++ loader.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name Fantastic Ultimate Solution to Addon Management Loader
 // @namespace https://www.bondageprojects.com/
-// @version 1.0.6
+// @version 1.0.7
 // @description A loader for the FUSAM
 // @author Sidious
+// @updateURL https://sidiousious.gitlab.io/bc-addon-loader/loader.user.js
 // @downloadURL https://sidiousious.gitlab.io/bc-addon-loader/loader.user.js
 // @match https://*.bondageprojects.elementfx.com/R*/*
 // @match https://*.bondage-europe.com/R*/*
 // @match https://*.bondageeurope.com/R*/*
 // @match https://*.bondageprojects.com/R*/*
 // @match https://*.bondage-asia.com/club/R*
-// @match http://localhost:*/*
+// @include /^http://localhost:[^/]*?/.*?$/
 // @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant none
 // @run-at document-end
